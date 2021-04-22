18fa8938e8fc7fd03fa524005cc32ce0492f5ed0
Masaya Misaki <mmisaki@libr-00019285.laureateinstitute.org>
2021-04-22 09:19:34 -0500

    shell script usage

--- run_fastSeg.sh
+++ run_fastSeg.sh
@@ -10,11 +10,12 @@ conda activate fastsurfer_gpu
 # Run fastSeg
 # cmd_dir=$FASTSEG_PATH
 #cmd_dir=$(cd $(dirname $0); pwd)  # The same directory as this file
-
 if [ "$#" -eq 1 ]; then
     $FASTSEG_DIR/fastSeg.py $1
 elif [ "$#" -eq 2 ]; then
     $FASTSEG_DIR/fastSeg.py $1 -o $2
-else
+elif [ "$#" -eq 3 ]; then
     $FASTSEG_DIR/fastSeg.py $1 -o $2 --batch_size $3
+else
+    echo "usage: run_fastSeg.py input_file [OUTPUT_PREFIX] [BATCH_SIZE]"
 fi
